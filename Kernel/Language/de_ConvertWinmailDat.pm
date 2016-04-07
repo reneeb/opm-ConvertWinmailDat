e541985dc72bc36b4dc7793f8d9576990886414b
reneeb <github@renee-baecker.de>
2016-04-07 09:00:15 +0200

    add german translations

--- Kernel/Language/de_ConvertWinmailDat.pm
+++ Kernel/Language/de_ConvertWinmailDat.pm
@@ -1,6 +1,6 @@
 # --
 # Kernel/Language/de_ConvertWinmailDat.pm - the German translation of ConvertWinmailDat
-# Copyright (C) 2015 Perl-Services, http://www.perl-services.de
+# Copyright (C) 2015 - 2016 Perl-Services, http://www.perl-services.de
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,15 +22,15 @@ sub Data {
     return if ref $Lang ne 'HASH';
 
     $Lang->{'Convert TNEF attachments'} = 'Konvertiere TNEF-Anhänge';
-    $Lang->{'Enable Debugging for ConvertWinmailDat.'} = '';
-    $Lang->{'No'} = '';
-    $Lang->{'Yes'} = '';
-    $Lang->{'Module to convert winmail.dat attachments.'} = '';
-    $Lang->{'Add "convert" button to article menu.'} = '';
+    $Lang->{'Enable Debugging for ConvertWinmailDat.'} = 'Debug-Modus für ConvertWinmailDat einschalten.';
+    $Lang->{'No'} = 'Nein';
+    $Lang->{'Yes'} = 'Ja';
+    $Lang->{'Module to convert winmail.dat attachments.'} = 'Modul zum umwandeln von winmail.dat-Anhängen';
+    $Lang->{'Add "convert" button to article menu.'} = 'Fügt einen "umwandeln"-Button zum Artikel-Menü';
     $Lang->{'Frontend module registration for the AgentTicketConvertWinmailDat module.'} =
         'Frontendmodul-Registration für das AgentTicketConvertWinmailDat Modul.';
-    $Lang->{'Convert winmail.dat attachment.'} = '';
-    $Lang->{'Convert winmail.dat'} = '';
+    $Lang->{'Convert winmail.dat attachment.'} = 'Wandele winmail.dat-Anhang um.';
+    $Lang->{'Convert winmail.dat'} = 'Wandele winmail.dat um';
 
     return 1;
 }
